477cb68c2d4965571bdc6d671cd21c208f2f1787
omeralon <omeralon3@gmail.com>
2018-05-31 20:10:23 +0300

    code first code

---
+++ Project-Ruty unity/Assets/code/expo01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+            
+public class expo01 : MonoBehaviour {
+    static string nextSecene = "01_expo_2";
+    static float audio_length = 9;
+    private float start_time;
+
+
+	// Use this for initialization
+	void Start () {
+        start_time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - start_time > audio_length)
+        {
+            SceneManager.LoadScene(nextSecene);
+        }
+	}
+}
