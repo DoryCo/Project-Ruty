087bb1d3b7327d72a79423dd2e41432ad9728fe6
omeralon <omeralon3@gmail.com>
2018-05-31 23:41:45 +0300

    codes demo

--- Project-Ruty unity/Assets/code/expo01.cs
+++ Project-Ruty unity/Assets/code/expo01.cs
@@ -5,20 +5,30 @@ using UnityEngine.SceneManagement;
             
 public class expo01 : MonoBehaviour {
     static string nextSecene = "01_expo_2";
-    static float audio_length = 9;
-    private float start_time;
 
+    //static float audio_length = 9;
+    //private float start_time;
+
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
-        start_time = Time.time;
+        //start_time = Time.time;
+
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time - start_time > audio_length)
+        //if (Time.time - start_time > audio_length)
+        //{
+        //    SceneManager.LoadScene(nextSecene);
+        //}
+
+        if (!audioSource.isPlaying)
         {
             SceneManager.LoadScene(nextSecene);
         }
+
 	}
 }
---
+++ Project-Ruty unity/Assets/code/expo_2_arr_r.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class expo_2_arr_r : MonoBehaviour
+{
+
+    static string nextSecene = "01_expo_3";
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Pressed left click, casting ray.");
+            CastRay();
+        }
+    }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit)
+        {
+            print(hit.collider.gameObject.name);
+            if (hit.collider.gameObject.name == "ArrowRight_01")
+            {
+                SceneManager.LoadScene(nextSecene);
+            }
+        }
+    }
+}
